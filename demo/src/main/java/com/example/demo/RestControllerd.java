1a41dae28f2a64258e964af900b70d7c3bf0a3aa
luisfeliz2 <luisfelizbeltre@gmail.com>
2024-02-04 11:23:14 +0100

    quizz con base de datos

---
+++ demo/src/main/java/com/example/demo/RestControllerd.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerd {
+	 @Autowired
+	    private ResultadoRepository resultadoRepository;
+
+	    @GetMapping("/resultados")
+	    public List<Resultado> obtenerResultados() {
+	        return resultadoRepository.findAll(); // Puedes ordenar o limitar la cantidad de resultados según tus necesidades
+	    }
+}
---
+++ demo/src/main/java/com/example/demo/Resultado.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Resultado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String nombreJugador;
+    
+    private int puntuacion;
+    private String categoria;
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombreJugador() {
+		return nombreJugador;
+	}
+
+	public void setNombreJugador(String nombreJugador) {
+		this.nombreJugador = nombreJugador;
+	}
+
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+    // Getters y setters
+}
